dde958d84bb13877533ecd64d00b2445b53b4a62
Chungking-express <rla1wlsdn@naver.com>
2020-06-27 13:40:43 +0900

    error

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chunkingbook/Desktop/JW/멋쟁이 사자처럼 8기 for Mac/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
